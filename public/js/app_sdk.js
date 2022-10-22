30f84093dcb7af115476467cc40ae71646d12ac2
rezki wissem <wissemrezki@gmail.com>
2022-10-22 12:24:45 +0200

    Version CookieV6

---
+++ public/js/app_sdk.js
@@ -0,0 +1,11 @@
+window.axeptioSettings = {
+    clientId: "6352bec6c85472350196d292",
+};
+
+(function(d, s) {
+    var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+    e.async = true;
+    e.src = "https://static.axept.io/sdk.js";
+    t.parentNode.insertBefore(e, t);
+})(document, "script");
+
