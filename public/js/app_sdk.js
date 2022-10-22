7e94583335468bf686e9de1c8d8e70cfb24e52a6
rezki wissem <wissemrezki@gmail.com>
2022-10-22 14:17:00 +0200

    Version CookieV9

--- public/js/app_sdk.js
+++ public/js/app_sdk.js
@@ -1,10 +1,10 @@
-window.axeptioSettings = {
-    clientId: "6353c9f4f0df4fd1cd738a1b",
-    cookiesVersion: "inginus/-fr",
-};
+// window.axeptioSettings = {
+//     clientId: "6353c9f4f0df4fd1cd738a1b",
+//     cookiesVersion: "inginus/-fr",
+// };
  
-(function(d, s) {
-    var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-    e.async = true; e.src = "//static.axept.io/sdk.js";
-    t.parentNode.insertBefore(e, t);
-})(document, "script");
+// (function(d, s) {
+//     var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+//     e.async = true; e.src = "//static.axept.io/sdk.js";
+//     t.parentNode.insertBefore(e, t);
+// })(document, "script");
